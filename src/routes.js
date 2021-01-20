dbc5c05d2254bd90f50ed4809e38381f6aab8fd9
Raissa Vieira <raissavieira125@gmail.com>
2021-01-19 22:42:48 -0300

    refactor: Edicao dos models index e user

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,12 @@
 const routes = require('express').Router();
+const { User } = require('./app/models')
 
 // Definicao das rotas
+
+User.create({
+    name: 'Raissa',
+    email: 'raissa@vieira',
+    password_hash: '1234567890'
+})
 
 module.exports = routes;
