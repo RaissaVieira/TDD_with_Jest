bf93acfde1ec3e68a2b015539b2c6239f27963db
Raissa Vieira <raissavieira125@gmail.com>
2021-01-22 09:58:08 -0300

    refactor: Adicionando novas rotas

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,15 @@
 const routes = require('express').Router();
 const SessionController = require('./app/controllers/SessionController')
+const auth = require('./app/middleware/auth');
 
 // Definicao das rotas
 
 routes.post('/sessions', SessionController.store);
+
+routes.use(auth);
+
+routes.get('/dashboard', (req, res) => {
+    return res.status(200).send();
+})
 
 module.exports = routes;
